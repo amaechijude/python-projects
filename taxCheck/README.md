700e6f9185c0b23b9db5798a68fa0910b548694d
amaechijude <ugwuamaechi5@gmail.com>
2023-09-04 08:30:00 +0100

    can respond with lowercase n,y

--- taxCheck/README.md
+++ taxCheck/README.md
@@ -1,3 +1,6 @@
+#Simple project to test multiple if statement
+
 1. Welcome message
-2. Ask if it is a startup and if yes give tax rate as 0.
+2. Ask if it is a startup and if yes. they ate eligible for tax exemption
 3. If not, then issue tax rate for different income levels.
+4. print the income, tax rate and compute the exact tax fee.
--- taxCheck/main.py
+++ taxCheck/main.py
@@ -1,38 +1,38 @@
-print("Welcome to tax rate Calculator")
+print("Welcome to tax rate Calculator\n")
 income = int(input("Input your gross income: $"))
 biz = str(input("Are you a startup? Reply Y for yes or N for no: "))
 #taxRate = 0
 
 #logic
 
-if biz == "N":
+if biz == "N" or biz == "n":
     if income <= 11_000:
         taxRate = 10
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax")
-    if income <= 44_725:
+    elif income <= 44_725:
         taxRate = 12
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax")
-    if income <= 95_375:
+    elif income <= 95_375:
         taxRate = 22
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax fees")
-    if income <= 182_100:
+    elif income <= 182_100:
         taxRate = 24
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax fees")
-    if income <= 231_250:
+    elif income <= 231_250:
         taxRate = 32
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax fees")
-    if income <= 578_125:
+    elif income <= 578_125:
         taxRate = 35
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax fees")
-    if income > 578_125:
+    elif income > 578_125:
         taxRate = 37
         tax = round(float((taxRate/100)*income), 2)
         print(f"Your income is ${income} and your tax rate is {taxRate}%, you should pay ${tax} in tax fees")
-elif biz == "Y":
+elif biz == "Y" or biz == "y":
     print("You are eligible for tax exemption")
