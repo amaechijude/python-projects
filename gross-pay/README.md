8e2fee7daf4a2017652ceba0daa8df938ecb01bc
amaechijude <ugwuamaechi5@gmail.com>
2023-09-05 07:54:26 +0100

    Increased rate over 40hours by 1.5

--- gross-pay/README.md
+++ gross-pay/README.md
@@ -1,2 +1,4 @@
 1. Write a program to prompt the user for work hours and rate per hour to compute gross pay.
-2. The result ha to be in 2 decimal place.
+2. The result ha to be in 2 decimal place
+#Addition
+3. Increase the rate for extra hours above 40 by 1.5.
--- gross-pay/main.py
+++ gross-pay/main.py
@@ -1,6 +1,10 @@
 print("Welcome to gross pay calculator")
 hour = int(input("Input your average work hours per day: "))
 rate = float(input("Input your average wage rate per hour: "))
-result = round(float(hour*rate), 2)
-#result = format(float(hour*rate), '.2f')
-print("Your average gross pay per day is","$"+str(result))
+if hour <= 40:
+    result = round(float(hour*rate), 2)
+    print(f"Your average gross pay per day is ${result}")
+else:
+    extra = (hour-40)*rate*1.5
+    result = round(float((40*rate)+(extra)), 2)
+    print(f"Your average gross pay per day is ${result}")
