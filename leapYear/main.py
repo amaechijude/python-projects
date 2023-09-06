1c10225c8003e635733d37e24e5096818186c50a
amaechijude <ugwuamaechi5@gmail.com>
2023-09-06 18:27:43 +0100

    leap year

---
+++ leapYear/main.py
@@ -0,0 +1,13 @@
+print("Welcome to leap year checker")
+year = int(input("Enter any year to check: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+    else:
+        print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
