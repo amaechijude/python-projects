f3aa6f7c24150f79941acc9c0037fb1d5971cdc0
Amaechi <111006036+amaechijude@users.noreply.github.com>
2023-09-08 08:35:02 +0100

    Update main.py

--- scoreChecker/main.py
+++ scoreChecker/main.py
@@ -9,17 +9,17 @@ except:
     print("Try again")
     quit()
 
-if score >= 0 and score < 40:
+if score < 40:
     print(f"Your score is {score} and your grade is F")
-elif score >= 40 and score < 45:
+elif score < 45:
     print(f"Your score is {score} and your grade is E")
-elif score >= 45 and score < 50:
+elif score < 50:
     print(f"Your score is {score} and your grade is D")
-elif score >= 50 and score < 60:
+elif score < 60:
     print(f"Your score is {score} and your grade is C")
-elif score >= 60 and score < 70:
+elif score < 70:
     print(f"Your score is {score} and your grade is B")
-elif score >= 70 and score <= 100:
+elif score <= 100:
     print(f"Your score is {score} and your grade is A")
 else:
     print("Your score is out of range")
