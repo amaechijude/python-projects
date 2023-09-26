afc4105ebb6399dbc4a79f3f318e2a98a9a39e90
amaechijude <ugwuamaechi5@gmail.com>
2023-09-26 12:49:36 +0100

    Made them a function

--- sum_of_digits/app.py
+++ sum_of_digits/app.py
@@ -1,7 +1,11 @@
-digit = input("Input digits: ")
-soln = 0
-param = []
-for num in digit:
-    soln += int(num)
-    param.append(int(num))
-print(f"The sum total of {param} is = {soln}.\n\t Total = {soln}")
+def sum_of_numbers(digit):
+    """Sums all integers"""
+    soln = 0
+    param = []
+    for num in digit:
+        soln += int(num)
+        param.append(int(num))
+    return f"The sum total of {param} is = {soln}.\n\t Total = {soln}"
+
+x = input("Input digits: ")
+print(sum_of_numbers(x))
--- sum_of_digits/main.py
+++ sum_of_digits/main.py
@@ -1,10 +1,14 @@
-digit = input("Input digits: ")
-index = 0
-soln = 0
-param = []
-while index < len(digit):
-    num = int(digit[index])
-    soln += num 
-    index += 1
-    param.append(num)
-print(f"The sum total of {param} is = {soln}.\n\t Total = {soln}")
+def sum_of_numbers(digit):
+    """Sums all integers"""
+    index = 0
+    soln = 0
+    param = []
+    while index < len(digit):
+        num = int(digit[index])
+        soln += num 
+        index += 1
+        param.append(num)
+    return f"The sum total of {param} is = {soln}.\n\t Total = {soln}"
+
+x = input("Input digits: ")
+print(sum_of_numbers(x))
