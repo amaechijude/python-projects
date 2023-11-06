4005b8e25384810fe0937c8c49b758a9162372fa
amaechijude <ugwuamaechijude5@gmail.com>
2023-11-06 11:12:45 +0100

    ospath

--- os-module/path.py
+++ os-module/path.py
@@ -2,5 +2,9 @@ import os
 abs_path = os.path.abspath('main.py')
 print(f"The absolute path is: {abs_path}")
 
-print(os.path.isabs(abs_path))
-print(f"{os.path.getsize(abs_path)} bytes")
+print(os.path.join('folder1', 'folder2', 'logo.png'))
+directory_name = os.path.dirname("/data/data/com.termux/files/home/python-projects/os-module/main.py")
+base_name = os.path.basename("/data/data/com.termux/files/home/python-projects/os-module/main.py")
+
+print(directory_name)
+print(base_name)
